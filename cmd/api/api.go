87678fb3d9fde9d76a7d49a6d6acd2e8d41b1221
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-04-23 13:54:21 +0545

    feat: create users and post function implemented

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Ay-afk-stack/gopher-socials/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
 	config config
+	store store.Storage
 }
 
 type config struct {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/Ay-afk-stack/gopher-socials/internal/env"
+	"github.com/Ay-afk-stack/gopher-socials/internal/store"
 	"github.com/joho/godotenv"
 )
 
@@ -16,8 +17,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 		}
 
 	mux := app.mount()
