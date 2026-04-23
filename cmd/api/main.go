1850a877786578a1b5ff410cce10396553f36dca
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-04-23 13:02:12 +0545

    feat: configured env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Ay-afk-stack/gopher-socials/internal/env"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file: ", err)
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Ay-afk-stack/gopher-socials
 
 go 1.26.2
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
